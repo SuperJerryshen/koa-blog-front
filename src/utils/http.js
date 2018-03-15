eff44f703987c06dc54ff1ec03ce91f168abab5a
沈经纬 <shenjingwei@shenjingweideMacBook-Pro.local>
2018-03-15 19:49:24 +0800

    修改正式环境中请求的url

--- src/utils/http.js
+++ src/utils/http.js
@@ -6,7 +6,7 @@ import { TOKEN_KEY } from './const';
 // 设置默认 host
 axios.defaults.baseURL = dev
   ? 'http://localhost:8080/api'
-  : 'http://me.jerryshen.cn/api';
+  : 'http://api.jerryshen.cn/blog/api';
 
 // 设置请求验证 Authorization
 const token = store.get(TOKEN_KEY);
