e57b1697168ba25cf95a9586d3d35636a762f95b
沈经纬 <327538014@qq.com>
2018-03-17 14:53:33 +0800

    样式微调 | 修复主页title问题

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -10,6 +10,8 @@ import {
 } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { map } from 'lodash';
+
+import { Page } from '../../components';
 import http from '../../utils/http';
 
 class Home extends Component {
@@ -33,63 +35,65 @@ class Home extends Component {
 
   render() {
     return (
-      <Segment loading={this.state.loading} basic>
-        <Grid>
-          <Grid.Column computer={16} mobile={16}>
-            <Item.Group divided>
-              {map(this.state.data, (item, index) => (
-                <Item
-                  key={index}
-                  style={{
-                    position: 'relative',
-                  }}
-                >
-                  <Item.Content>
-                    <div
-                      style={{
-                        position: 'absolute',
-                        bottom: 20,
-                        right: 0,
-                      }}
-                    >
-                      <Label image size="medium">
-                        <img src={item.author.avatar} />
-                        <span
-                          style={{
-                            maxWidth: 64,
-                            overflow: 'hidden',
-                            display: 'inline-block',
-                          }}
-                        >
-                          {item.author.nickname}
-                        </span>
-                      </Label>
-                    </div>
-                    <Item.Header as={Link} to={`/post/${item._id}`}>
-                      {item.title}
-                    </Item.Header>
-                    <Item.Meta>
-                      {new Date(item.created_time).toLocaleDateString()}
-                    </Item.Meta>
-                    <Item.Description>
-                      <Label>
-                        <Icon name="like" /> {item.stared_user.length}
-                      </Label>
-                      <Label>
-                        <Icon name="eye" /> {item.viewed_times}
-                      </Label>
-                      <Label>
-                        <Icon name="comment" /> {item.comment.length}
-                      </Label>
-                    </Item.Description>
-                    <Item.Extra>{item.create_time}</Item.Extra>
-                  </Item.Content>
-                </Item>
-              ))}
-            </Item.Group>
-          </Grid.Column>
-        </Grid>
-      </Segment>
+      <Page title="经纬天地">
+        <Segment loading={this.state.loading} basic>
+          <Grid>
+            <Grid.Column computer={16} mobile={16}>
+              <Item.Group divided>
+                {map(this.state.data, (item, index) => (
+                  <Item
+                    key={index}
+                    style={{
+                      position: 'relative',
+                    }}
+                  >
+                    <Item.Content>
+                      <div
+                        style={{
+                          position: 'absolute',
+                          bottom: 20,
+                          right: 0,
+                        }}
+                      >
+                        <Label image size="medium">
+                          <img src={item.author.avatar} />
+                          <span
+                            style={{
+                              maxWidth: 64,
+                              overflow: 'hidden',
+                              display: 'inline-block',
+                            }}
+                          >
+                            {item.author.nickname}
+                          </span>
+                        </Label>
+                      </div>
+                      <Item.Header as={Link} to={`/post/${item._id}`}>
+                        {item.title}
+                      </Item.Header>
+                      <Item.Meta>
+                        {new Date(item.created_time).toLocaleDateString()}
+                      </Item.Meta>
+                      <Item.Description>
+                        <Label>
+                          <Icon name="like" /> {item.stared_user.length}
+                        </Label>
+                        <Label>
+                          <Icon name="eye" /> {item.viewed_times}
+                        </Label>
+                        <Label>
+                          <Icon name="comment" /> {item.comment.length}
+                        </Label>
+                      </Item.Description>
+                      <Item.Extra>{item.create_time}</Item.Extra>
+                    </Item.Content>
+                  </Item>
+                ))}
+              </Item.Group>
+            </Grid.Column>
+          </Grid>
+        </Segment>
+      </Page>
     );
   }
 }
